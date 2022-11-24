c6a7051b6055466b6cd34886a369278fc93487d6
Maximiliano <brotzumaxx@gmail.com>
2022-11-23 22:32:24 -0300

    Proyecto con Ejs

---
+++ Clase-10-Pug-&-Ejs/Ejs/server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+
+
+
+
+const productos = [];
+
+function getLastId() {
+    if (productos.length === 0) {
+        return 0;
+    }
+
+    const lastId = productos[(productos.length - 1)].id;
+    return lastId;
+}
+
+
+
+
+app.get("/", (req, res) => {
+    res.render("form");
+});
+
+app.post("/productos", (req, res) => {
+    const producto = req.body;
+    const idAsignado = getLastId() + 1;
+    producto.id = idAsignado;
+    productos.push(producto);
+
+    res.redirect("/");
+});
+
+app.get("/productos", (req, res) => {
+    const listEmpty = productos.length === 0;
+    res.render("products", { productos: productos, notEmpty: !listEmpty });
+});
+
+app.listen(8080, () => {
+    console.log("Servidor escuchando en puerto 8080");
+})
